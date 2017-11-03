112734a27dab2cf072a9d5cd07d450f5b7de71f3
Angel Arciga <31705013+laangelita22@users.noreply.github.com>
2017-11-02 21:05:20 -0600

    Took out hardcode hand.

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var Game = (function () {
 
 	function Game() {
 		// ADD TRUE back inside deck!!! When done HARDCODEING Hands
-		this.deck = new Deck();
+		this.deck = new Deck(true);
 		this.player = new Player(1000);
 		this.hand = new Hand();
 		this.newHand = true;
